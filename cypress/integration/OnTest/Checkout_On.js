4a42567c455a1b26c02349959b1ce8c9b2ab99f9
alishachawla06 <alishabe@gmail.com>
2022-06-30 23:12:21 +0200

    Updated Coments and applitools screenshots

--- cypress/integration/OnTest/Checkout_On.js
+++ cypress/integration/OnTest/Checkout_On.js
@@ -22,9 +22,6 @@ CheckOut.getcookieBanner().click()
 
   /* GIVEN user lands on the Homepage
    AND Clicks on the "Running shoes" */
-
-   
-
   it("Checkout", function () {
    
     CheckOut.getCategory().click()
@@ -34,25 +31,26 @@ CheckOut.getcookieBanner().click()
     CheckOut.getSize().click({ force: true })
     CheckOut.getADD().click().wait(3000)
 
-//Verify that product is added to the cart
+/* GIVEN product is added to the cart
+   When user clicks on the shopping bag icon" 
+   Then user is redirected to shopping bag page 
+   And url is changed*/
 
-Assertion.verifyCartisNotEmpty()
+   Assertion.verifyCartisNotEmpty()
+   CheckOut.getSB().click().wait(3000)
+   cy.url().should('include',this.data.aftersubmiturl)
 
-//    CheckOut.getcartadded().should('be.visible')
-
-    CheckOut.getSB().click().wait(3000)
-
-//Verify url changes when user clicks on Shopping bag icon
-
-    cy.url().should('include',this.data.aftersubmiturl)
-
+   /* Given user is at shopping bag page
+      When user clicks on checkout option
+      And user Enters email to proceed
+      Then redirected to the checkout page */
     CheckOut.getCheck().click().wait(5000)
-
-//verify url changes when user reaches register page
-
     CheckOut.getEmail().click().type(this.data.Email)
     CheckOut.getProceed().click().wait(5000)
-
+   /*  Given user is at checkout page
+       When enters all the valid user details 
+       And proceed
+       Then user is redirected to payment option page */ 
     CheckOut.getFname().click().type(this.data.Firstname)
     CheckOut.getLname().click().type(this.data.Lastname)
     CheckOut.getAddress().click().type(this.data.Address)
@@ -63,31 +61,23 @@ Assertion.verifyCartisNotEmpty()
     CheckOut.getPayment().click().wait(5000)
     CheckOut.getPaymentMethodCreditCard().click()
     cy.wait(3000)
+    /* Given user is at payment details page
+       When inputs valid card details
+       And proceeds
+       Then order is succesfully placed
+       */
     CheckOut.getCardNo().click().type(this.data.Card)
     CheckOut.getExpiryDate().click().type(this.data.date)
     CheckOut.getCVC().click().type(this.data.CVC)
-    
-    //cy.iframe('.adyen-checkout__card-input .adyen-checkout__input[data-cse="encryptedCardNumber"] iframe').find('#encryptedSecurityCode').click().type('223')
-   
-    //CheckOut.getDate().click().type(this.data.date)
-   // CheckOut.getCVC().click().type(this.data.CVC)
     CheckOut.getNameoncard().click().type(this.data.Nameoncard)
     CheckOut.getSubmitCard().click()
+    Assertion.verifyOrderConfirmationText(this.data.orderConfirmationText)
 
-   Assertion.verifyOrderConfirmationText(this.data.orderConfirmationText)
 
-   // cy.get('.confirmation-container__subtitle').should("contain.text","You'll receive a confirmation email shortly at ontest@gmail.com")
 
   })
 
-  /*it("Payment", function () {
 
-    
-    
-    cy.title().should('eq', this.data.pagetitle)
-
-
-  });*/
 
 
 
